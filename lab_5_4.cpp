ccc66cbe5d36b5503d516b610e6600f2167787e6
Unknown <jaranyaboo@cvc-cha.ac.th>
2017-12-01 14:41:40 +0700

    ทำข้อ 4

--- lab_5_4.cpp
+++ lab_5_4.cpp
@@ -10,7 +10,11 @@ int main() {
     cin >> menu;
 
     switch (menu) {
-        case 1: cout << "Call Program 1";
+        case 1: cout << "Call Program 1" << endl; break;
+        case 2: cout << "Call Program 2" << endl; break;
+        case 3: cout << "Call Program 3" << endl; break;
+        case 4: cout << "Call Program 4" << endl; break;
+        case 5: cout << "Call Program 5" << endl; break;
     }
 
     return 0;
